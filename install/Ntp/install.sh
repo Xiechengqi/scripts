8d4bcd8fc6800e71b4369be5b3f31bd1e69441de
Xiechengqi <Xiechengqi01@gmail.com>
2021-07-26 22:20:44 +0800

    Update install.sh

--- install/Ntp/install.sh
+++ install/Ntp/install.sh
@@ -40,14 +40,17 @@ function main() {
 # environment
 serviceName="ntp"
 
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0
+
 # install
 EXEC "apt-get update && apt-get install -y gnupg2 curl software-properties-common $serviceName"
 
 # start
 EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
 
-# check ntp
-EXEC "ntpq -4c rv | grep leap_none"
+# check
+EXEC "ntpq -4c rv | grep leap_none" && ntpq -4c rv | grep leap_none
 }
 
 main
