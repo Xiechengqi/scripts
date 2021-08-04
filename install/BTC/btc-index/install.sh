3dd38218f76ea8946bb1c05b78c9dcc4085c8133
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-04 13:14:11 +0800

    Update install.sh

--- install/BTC/btc-index/install.sh
+++ install/BTC/btc-index/install.sh
@@ -2,13 +2,19 @@
 
 #
 # xiechengqi
-# 2021/07/23
+# 2021/08/03
 # https://github.com/bitpay/bitcore
-# Ubuntu 16+
+# Ubuntu 18.04
 # compile install bitcore
 #
 
 source /etc/profile
+
+OS() {
+osType=$1
+osVersion=$2
+curl -SsL https://raw.githubusercontent.com/Xiechengqi/scripts/master/tool/os.sh | bash -s ${osType} ${osVersion} || exit 1
+}
 
 INFO() {
 printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
@@ -39,6 +45,12 @@ fi
 }
 
 function main() {
+# check os
+OS "ubuntu" "18"
+
+# get net option
+[ "$1" = "mainnet" ] && net="mainnet" || net="testnet"
+
 # environment
 serviceName="btc-index"
 version="8.25.12"
@@ -46,32 +58,23 @@ installPath="/data/BTC/${serviceName}-${version}"
 downloadUrl="https://github.com/bitpay/bitcore/archive/refs/tags/v${version}.tar.gz"
 user="btc-index"   # 启动 bitcore 用户
 hostIp="127.0.0.1" # 安装 bitcoin 主机 ip
-rpcPort="18332"  # bitcoin 配置
-p2pPort="18333"   # bitcoin 配置
-rpcUser="btc-node"    # bitcoin 配置
-rpcPassword="local321"   # bitcoin 配置
+[ "$net" = "mainnet" ] && rpcPort="8332" || rpcPort="18332"  # 同 bitcoin 配置
+[ "$net" = "mainnet" ] && p2pPort="8333" || p2pPort="18333"  # 同 bitcoin 配置
+rpcUser="bitcoin"    # 同 bitcoin 配置
+rpcPassword="local321"   # 同 bitcoin 配置
 
 # check service
 systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0
 
 # install script url
-bitcoinUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/BTC/Bitcoin/install.sh"
 mongodbUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Mongodb/install.sh"
 nodeUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Node/install.sh"
-pythonUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Python/install.sh"
-
-# install bitcoin
-local netOption=${1-"testnet"}
-curl -SsL $bitcoinUrl | bash -s ${netOption}
 
 # install mongodb
 curl -SsL $mongodbUrl | bash
 
 # install node
-curl -SsL $nodeUrl | bash
-
-# install python
-curl -SsL $pythonUrl | bash -s 3.5
+curl -SsL $nodeUrl | bash -s 12.16.0
 
 # install gcc
 gcc --version &> /dev/null || EXEC "apt update && apt install -y build-essential"
@@ -95,21 +98,21 @@ INFO "su $user -c 'cd $installPath && npm install'"
 su $user -c "cd $installPath && npm install"
 
 # config
-cat > $installPath/${serviceName}.config.json << EOF
+cat > $installPath/bitcore.config.json << EOF        # 配置文件名不可修改
 {
   "bitcoreNode": {
     "chains": {
       "BTC": {
-        "${netOption}": {
+        "$net": {
           "chainSource": "p2p",
           "trustedPeers": [
             {
-              "host": "${hostIp}",
+              "host": "$hostIp",
               "port": $p2pPort 
             }
           ],
           "rpc": {
-            "host": "${hostIp}",
+            "host": "$hostIp",
             "port": $rpcPort,
             "username": "$rpcUser",
             "password": "$rpcPassword"
@@ -146,6 +149,7 @@ cat > /lib/systemd/system/${serviceName}.service << EOF
 [Unit]
 Description=A full stack for bitcoin and blockchain-based applications
 Documentation=https://github.com/bitpay/bitcore
+After=network.target
 
 [Service]
 User=$user
@@ -169,7 +173,7 @@ EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName
 # info
 YELLOW "version: $version"
 YELLOW "install path: $installPath"
-YELLOW "config path: $installPath/${serviceName}.config.json"
+YELLOW "config path: $installPath/bitcore.config.json"
 YELLOW "log path: $installPath/logs"
 YELLOW "rpcUser: $rpcUser"
 YELLOW "rpcPassword: $rpcPassword"
