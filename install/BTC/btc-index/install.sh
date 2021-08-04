c36f4ed5a8d6b0eff3058a282b75100e032aa793
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-04 15:16:13 +0800

    Update install.sh

--- install/BTC/btc-index/install.sh
+++ install/BTC/btc-index/install.sh
@@ -3,12 +3,23 @@
 #
 # xiechengqi
 # 2021/08/03
-# https://github.com/bitpay/bitcore
-# Ubuntu 18.04
-# compile install bitcore
+# github: https://github.com/Adamant-im/ETH-transactions-storage
+# OS: ubuntu 18.04
+# install ETH index
+# prerequisites
+#   geth or openethereum (with currently synchronized chain)
+#   Python 3.6
+#   Postgresql 10.5 (https://github.com/postgres/postgres)
+#   Postgrest for API (https://github.com/PostgREST/postgrest)
 #
 
 source /etc/profile
+
+trap "_clean" EXIT
+
+_clean() {
+cd /tmp && rm -f $$.tar
+}
 
 OS() {
 osType=$1
@@ -44,116 +55,176 @@ ERROR "Execution command (${cmd}) failed, please check it and try again."
 fi
 }
 
+function install_postgrest() {
+# environments
+local serviceName="postgrest"
+local version="7.0.1"
+local installPath="/data/${serviceName}-${version}"
+local downloadUrl="https://github.com/PostgREST/postgrest/releases/download/v${version}/postgrest-v${version}-linux-x64-static.tar.xz"
+local port="3000"
+local postgresPort="5432"
+
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0 
+
+# check install path
+EXEC "rm -rf $installPath"
+EXEC "mkdir -p $installPath/{conf,logs}"
+
+# download tarball
+EXEC "curl -SsL $downloadUrl | xz -d > /tmp/$$.tar"
+EXEC "tar xf /tmp/$$.tar -C $installPath"
+
+# register bin
+EXEC "ln -fs $installPath/* /usr/bin/"
+EXEC "which postgrest"
+
+# conf
+cat > $installPath/conf/postgrest.conf << EOF
+db-uri = "postgres://$dbUser:$dbPassword@127.0.0.1:$postgresPort/$dbName"
+db-schema = "public"
+db-anon-role = "eth"
+db-pool = 10
+server-host = "0.0.0.0"
+server-port = $port
+EOF
+
+# create start.sh
+cat > $installPath/start.sh << EOF
+#!/usr/bin/env bash
+source /etc/profile
+export LD_LIBRARY_PATH=/data/postgres/lib
+
+postgrest $installPath/conf/postgrest.conf &> $installPath/logs/$(date +%Y%m%d%H%M%S).log
+EOF
+EXEC "chmod +x $installPath/start.sh"
+
+# register service
+cat > /lib/systemd/system/${serviceName}.service << EOF
+[Unit]
+Description=Transaction API with Postgrest
+After=network.target
+After=postgres.target
+
+[Service]
+User=root
+Group=root
+ExecStart=/bin/bash $installPath/start.sh
+ExecStop=/bin/kill -s QUIT $MAINPID
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# change softlink
+EXEC "ln -fs $installPath $(dirname $installPath)/$serviceName"
+
+# start
+EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
+EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
+
+# info
+YELLOW "$serviceName is running ..."
+}
+
 function main() {
+
 # check os
 OS "ubuntu" "18"
 
 # get net option
 [ "$1" = "mainnet" ] && net="mainnet" || net="testnet"
 
-# environment
-serviceName="btc-index"
-version="8.25.12"
-installPath="/data/BTC/${serviceName}-${version}"
-downloadUrl="https://github.com/bitpay/bitcore/archive/refs/tags/v${version}.tar.gz"
-user="btc-index"   # 启动 bitcore 用户
-hostIp="127.0.0.1" # 安装 bitcoin 主机 ip
-[ "$net" = "mainnet" ] && rpcPort="8332" || rpcPort="18332"  # 同 bitcoin 配置
-[ "$net" = "mainnet" ] && p2pPort="8333" || p2pPort="18333"  # 同 bitcoin 配置
-rpcUser="bitcoin"    # 同 bitcoin 配置
-rpcPassword="local321"   # 同 bitcoin 配置
+# environments
+local serviceName="eth-index"
+local version="master"
+local installPath="/data/ETH/${serviceName}-${version}"
+local downloadUrl="https://github.com/Xiechengqi/wx-eth-indexer.git"
+local nodeIp="127.0.0.1"
+local port="8545"
+local user="postgres"
+local dbIp="127.0.0.1"
+local dbName="eth"
+local dbUser="eth"
+local dbPassword="eth"
+local startBlockNumber="6520000"
 
-# check service
-systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0
+# install scripts url
+postgresUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Postgres/install.sh"
 
-# install script url
-mongodbUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Mongodb/install.sh"
-nodeUrl="https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Node/install.sh"
-
-# install mongodb
-curl -SsL $mongodbUrl | bash
-
-# install node
-curl -SsL $nodeUrl | bash -s 12.16.0
-
-# install gcc
-gcc --version &> /dev/null || EXEC "apt update && apt install -y build-essential"
-EXEC "gcc --version" && gcc --version
-EXEC "g++ --version" && g++ --version
+# check
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0 
 
 # check install path
-[ ! -d $installPath ] && EXEC "mkdir -p $installPath/logs"
+EXEC "rm -rf $installPath"
+EXEC "mkdir -p $installPath/logs"
 
-# check user
-! cat /etc/passwd | grep $user &> /dev/null && EXEC "useradd -m $user"
+# download
+EXEC "git clone -b master $downloadUrl $installPath/src"
 
-# download tarball
-EXEC "curl -sSL $downloadUrl | tar zx --strip-components 1 -C $installPath"
+# install postgres
+curl -SsL $postgresUrl | bash
 
-# chown
-EXEC "chown -R $user:$user $installPath"
+# init pgsql
+cat > /home/$user/create.sql << EOF
+create database index;
+create database $dbName;
+EOF
+EXEC "su $user -c 'psql -f /home/$user/create.sql'"
+EXEC "su $user -c 'psql -f $installPath/src/create_table.sql $dbName'"
+cat > /home/$user/init.sql << EOF
+create user $dbUser with password '$dbPassword';
+GRANT ALL ON ethtxs TO $dbUser;
+GRANT ALL PRIVILEGES ON DATABASE index TO $dbUser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $dbUser;
+EOF
+EXEC "su $user -c 'psql -f /home/$user/init.sql $dbName'"
 
-# compile and install
-INFO "su $user -c 'cd $installPath && npm install'"
-su $user -c "cd $installPath && npm install"
+# install postgrest
+install_postgrest
+
+# install pip3
+! pip3 --version &>/dev/null && EXEC "export DEBIAN_FRONTEND=noninteractive" && EXEC "apt update && apt install -y python3-pip"
+
+# install python modules
+EXEC "pip3 install web3"
+EXEC "pip3 install psycopg2" 
 
 # config
-cat > $installPath/bitcore.config.json << EOF        # 配置文件名不可修改
-{
-  "bitcoreNode": {
-    "chains": {
-      "BTC": {
-        "$net": {
-          "chainSource": "p2p",
-          "trustedPeers": [
-            {
-              "host": "$hostIp",
-              "port": $p2pPort 
-            }
-          ],
-          "rpc": {
-            "host": "$hostIp",
-            "port": $rpcPort,
-            "username": "$rpcUser",
-            "password": "$rpcPassword"
-          }
-        }
-      }
-    },
-    "services": {
-      "api": {
-        "rateLimiter": {
-          "whitelist": [
-            "::ffff:127.0.0.1"
-          ]
-        }
-      }
-    }
-  }
-}
+cat > $installPath/src/config.ini << EOF
+[base]
+node_address = http://$nodeIp:$port
+log_file_path = $installPath/logs/${serviceName}.log
+start_block_number = $startBlockNumber 
+
+[db]
+host = $dbIp
+user = $dbUser 
+password = $dbPassword
 EOF
 
 # create start.sh
 cat > $installPath/start.sh << EOF
 #!/usr/bin/env bash
-export NODE_OPTIONS=--max_old_space_size=3145728
-cd $installPath && npm run node &> $installPath/logs/$(date +%Y%m%d%H%M%S).log
+source /etc/profile
+export LD_LIBRARY_PATH=/data/postgres/lib
+
+cd $installPath/src
+$(which python3.6) $installPath/src/ethsync.py $dbName
 EOF
 EXEC "chmod +x $installPath/start.sh"
-
-# chown
-EXEC "chown -R $user:$user $installPath"
 
 # register service
 cat > /lib/systemd/system/${serviceName}.service << EOF
 [Unit]
-Description=A full stack for bitcoin and blockchain-based applications
-Documentation=https://github.com/bitpay/bitcore
+Description=EthereumTransactionStorage
+After=syslog.target
 After=network.target
+After=postgrest.service
 
 [Service]
-User=$user
-Group=$user
 ExecStart=/bin/bash $installPath/start.sh
 ExecStop=/bin/kill -s QUIT \$MAINPID
 Restart=always
@@ -173,12 +244,9 @@ EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName
 # info
 YELLOW "version: $version"
 YELLOW "install path: $installPath"
-YELLOW "config path: $installPath/bitcore.config.json"
+YELLOW "conf path: $installPath/src/config.ini"
 YELLOW "log path: $installPath/logs"
-YELLOW "rpcUser: $rpcUser"
-YELLOW "rpcPassword: $rpcPassword"
-YELLOW "rpcPort: $rpcPort"
-YELLOW "p2pPort: $p2pPort"
+YELLOW "blockchain info cmd: "
 YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] $serviceName"
 }
 
