cb2580188ca74d2e1d7f40013fdcb93ca9f117bb
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-05 14:40:11 +0800

    Update install.sh

--- install/Redis/install.sh
+++ install/Redis/install.sh
@@ -56,7 +56,7 @@ downloadUrl="http://download.redis.io/releases/redis-${version}.tar.gz"
 systemctl is-active ${serviceName} &> /dev/null && YELLOW "${serviceName} is running ..." && return 0
 
 # check install path
-EXEC "rm -rf $installPath"
+EXEC "rm -rf $installPath $(dirname $installPath)/${serviceName}"
 EXEC "mkdir -p $installPath"
 
 # download tarball
