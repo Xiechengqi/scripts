5992fd2434e6bb724a11bdf580a3d15a5b9d47cd
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-09 16:00:57 +0800

    Update install.sh

--- install/Docker/install.sh
+++ install/Docker/install.sh
@@ -45,18 +45,32 @@ fi
 }
 
 main() {
+
+# check os
+OS "ubuntu"
+
+# environments
+serviceName="docker"
+
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0 
+
+# remove old apps
 EXEC "apt-get remove docker docker-engine docker.io containerd runc"
 
+# install requirements
 EXEC "apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release"
-    
-EXEC "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
 
+# add app source
+EXEC "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
 cat > /etc/apt/sources.list.d/docker.list << EOF
 deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
 EOF
 
+# install
 EXEC "apt-get update && apt-get -y install docker-ce docker-ce-cli containerd.io"
  
+# check docker
 EXEC "docker run hello-world"
 }
 
