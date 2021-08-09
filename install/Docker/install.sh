416cd1dbc407cfdd3728b7f66a9e3651d174c723
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-09 14:27:12 +0800

    Create install.sh

---
+++ install/Docker/install.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+#
+# xiechengqi
+# 2021/08/09
+# make install docker
+#
+
+source /etc/profile
+
+OS() {
+osType=$1
+osVersion=$2
+curl -SsL https://raw.githubusercontent.com/Xiechengqi/scripts/master/tool/os.sh | bash -s ${osType} ${osVersion} || exit 1
+}
+
+INFO() {
+printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "%s" "$1"
+printf "\n"
+}
+
+YELLOW() {
+printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "\033[33m%s\033[0m" "$1"
+printf "\n"
+}
+
+ERROR() {
+printf -- "\033[41;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "%s" "$1"
+printf "\n"
+exit 1
+}
+
+EXEC() {
+local cmd="$1"
+INFO "${cmd}"
+eval ${cmd} 1> /dev/null
+if [ $? -ne 0 ]; then
+ERROR "Execution command (${cmd}) failed, please check it and try again."
+fi
+}
+
+main() {
+EXEC "apt-get remove docker docker-engine docker.io containerd runc"
+
+EXEC "apt-get update && apt-get install apt-transport-https ca-certificates curl gnupg lsb-release"
+    
+EXEC "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
+
+cat > /etc/apt/sources.list.d/docker.list << EOF
+deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
+EOF
+
+EXEC "apt-get update && apt-get install docker-ce docker-ce-cli containerd.io"
+ 
+EXEC "docker run hello-world"
+}
+
+main $@
