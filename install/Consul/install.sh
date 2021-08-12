5fe782dacf513934a644a58e5e67c34aa7db1fba
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-12 13:20:27 +0800

    Create install.sh

---
+++ install/Consul/install.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+#
+# xiechengqi
+# 2021/08
+# install consul standalone
+#
+
+source /etc/profile
+
+OS() {
+osType=$1
+osVersion=$2
+curl -SsL https://raw.githubusercontent.com/Xiechengqi/scripts/master/tool/os.sh | bash -s ${osType} ${osVersion} || exit 1
+}
+
+INFO() {
+printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "%s" "$1"
+printf "\n"
+}
+
+YELLOW() {
+printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "\033[33m%s\033[0m" "$1"
+printf "\n"
+}
+
+ERROR() {
+printf -- "\033[41;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "%s" "$1"
+printf "\n"
+exit 1
+}
+
+EXEC() {
+local cmd="$1"
+INFO "${cmd}"
+eval ${cmd} 1> /dev/null
+if [ $? -ne 0 ]; then
+ERROR "Execution command (${cmd}) failed, please check it and try again."
+fi
+}
+
+main() {
+# environments
+serviceName="consul"
+version="1.10.1"
+installPath="/data/${serviceName}-${version}"
+downloadUrl="https://releases.hashicorp.com/consul/${version}/consul_${version}_linux_amd64.zip"
+
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName has been installed ..." && return 0
+
+# check install path
+EXEC "rm -rf $installPath $(dirname $installPath)/${serviceName}"
+EXEC "mkdir -p $installPath/{bin,conf,data,logs}"
+
+# install unzip
+EXEC "apt update && apt install -y unzip"
+
+# download
+EXEC "rm -rf /tmp/${serviceName} && mkdir /tmp/${serviceName}"
+EXEC "curl -SsL $downloadUrl -o /tmp/${serviceName}/${serviceName}.zip"
+EXEC "unzip /tmp/${serviceName}/${serviceName}.zip -d ${installPath}/bin"
+
+# register bin
+EXEC "ln -fs $installPath/bin/consul /usr/local/bin/consul"
+
+# create start.sh
+cat > $installPath/${serviceName}.service << EOF
+#!/usr/bin/env bash
+source /etc/profile
+
+timestamp=\$(date +%Y%m%d%H%M%S)
+touch $installPath/logs/\${timestamp}.log && ln -fs $installPath/logs/\${timestamp}.log $installPath/logs/latest.log
+consul agent -server -bootstrap-expect=1 -data-dir=$installPath/data -node=master -bind=127.0.0.1 -config-dir=$installPath/conf -client 0.0.0.0 -ui &> $installPath/logs/latest.log
+EOF
+
+# register service
+cat > $installPath/${serviceName}.service << EOF
+[Unit]
+Description=Consul
+Documentation=https://github.com/hashicorp/consul
+After=network.target
+
+[Service]
+User=root
+Group=root
+ExecStart=/bin/bash $installPath/start.sh
+ExecStop=/bin/kill -s QUIT \$MAINPID
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+EXEC "rm -f /lib/systemd/system/${serviceName}.service"
+EXEC "ln -fs $installPath/${serviceName}.service /lib/systemd/system/${serviceName}.service"
+
+# change softlink
+EXEC "ln -fs $installPath $(dirname $installPath)/$serviceName"
+
+# start
+EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
+EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
+
+# info
+YELLOW "${serviceName} version: $version"
+YELLOW "install path: $installPath"
+YELLOW "config path: $installPath/conf"
+YELLOW "data path: $installPath/data"
+YELLOW "tail log cmd: tail -f $installPath/logs/latest.log"
+YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] $serviceName"
+}
+
+main $@
