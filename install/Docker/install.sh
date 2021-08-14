c8d463bd505aaac63ff3227500564ce8239731cf
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-14 18:48:39 +0800

    Update install.sh

--- install/Docker/install.sh
+++ install/Docker/install.sh
@@ -11,9 +11,28 @@
 source /etc/profile
 
 OS() {
-osType=$1
-osVersion=$2
-curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/tool/os.sh | bash -s ${osType} ${osVersion} || exit 1
+if [ -f "/etc/debian_version" ]; then
+source /etc/os-release && local os="${ID}"
+elif [ -f "/etc/fedora-release" ]; then
+local os="fedora"
+elif [ -f "/etc/redhat-release" ]; then
+local os="centos"
+else
+exit 1
+fi
+
+if [ -f /etc/redhat-release ]; then
+local os_full=`awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release`
+elif [ -f /etc/os-release ]; then
+local os_full=`awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release`
+elif [ -f /etc/lsb-release ]; then
+local os_full=`awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release`
+else
+exit 1
+fi
+
+local main_ver="$( echo $os_full | grep -oE  "[0-9.]+")"
+printf -- "%s" "${os}${main_ver%%.*}"
 }
 
 INFO() {
@@ -44,18 +63,7 @@ ERROR "Execution command (${cmd}) failed, please check it and try again."
 fi
 }
 
-main() {
-
-# check os
-OS "ubuntu"
-
-# environments
-serviceName="docker"
-countryCode=`curl -SsL https://api.ip.sb/geoip | sed 's/,/\n/g' | grep country_code | awk -F '"' '{print $(NF-1)}'`
-
-# check service
-systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0
-
+_ubuntu() {
 # remove old apps
 apt-get remove docker docker-engine docker.io containerd runc &> /dev/null
 
@@ -78,6 +86,33 @@ fi
 
 # install
 EXEC "apt-get update && apt-get -y install docker-ce docker-ce-cli containerd.io"
+}
+
+_centos() {
+INFO "yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine" && yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+EXEC "yum install -y yum-utils device-mapper-persistent-data lvm2"
+EXEC "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+[ "${countryCode}" = "CN" ] && EXEC "sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo" && EXEC "yum makecache fast"
+EXEC "yum install docker-ce docker-ce-cli containerd.io"
+}
+
+main() {
+
+# get os info
+osInfo=`OS`
+
+# environments
+serviceName="docker"
+countryCode=`curl -SsL https://api.ip.sb/geoip | sed 's/,/\n/g' | grep country_code | awk -F '"' '{print $(NF-1)}'`
+
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0
+
+echo $osInfo | grep ubuntu &> /dev/null && _ubuntu
+echo $osInfo | grep centos &> /dev/null && _centos
+
+# start service
+EXEC "systemctl start docker"
 
 # check docker
 EXEC "docker run hello-world"
