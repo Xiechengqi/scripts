e21f5417dcedf5c31e47d71af411296aa8d17271
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-15 00:30:06 +0800

    Create common.sh

---
+++ tool/common.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#
+# xiechengqi
+# 2021/08
+# common shell functions and env
+#
+
+source /etc/profile
+
+# println information
+INFO() {
+printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "%s" "$1"
+printf "\n"
+}
+
+# println yellow color information
+YELLOW() {
+printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "\033[33m%s\033[0m" "$1"
+printf "\n"
+}
+
+# println error information
+ERROR() {
+printf -- "\033[41;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
+printf -- "%s" "$1"
+printf "\n"
+exit 1
+}
+
+# exec cmd and print error information
+EXEC() {
+local cmd="$1"
+INFO "${cmd}"
+eval ${cmd} 1> /dev/null
+if [ $? -ne 0 ]; then
+ERROR "Execution command (${cmd}) failed, please check it and try again."
+fi
+}
