478b9b3e5c04952426dc6e0e30639ff4a0d7d036
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-16 10:12:31 +0800

    Update install.sh

--- install/BTC/btc-index/install.sh
+++ install/BTC/btc-index/install.sh
@@ -9,42 +9,9 @@
 #
 
 source /etc/profile
+source <(curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/tool/common.sh)
 
-OS() {
-osType=$1
-osVersion=$2
-curl -SsL https://raw.githubusercontent.com/Xiechengqi/scripts/master/tool/os.sh | bash -s ${osType} ${osVersion} || exit 1
-}
-
-INFO() {
-printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
-printf -- "%s" "$1"
-printf "\n"
-}
-
-YELLOW() {
-printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
-printf -- "\033[33m%s\033[0m" "$1"
-printf "\n"
-}
-
-ERROR() {
-printf -- "\033[41;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
-printf -- "%s" "$1"
-printf "\n"
-exit 1
-}
-
-EXEC() {
-local cmd="$1"
-INFO "${cmd}"
-eval ${cmd} 1> /dev/null
-if [ $? -ne 0 ]; then
-ERROR "Execution command (${cmd}) failed, please check it and try again."
-fi
-}
-
-function main() {
+main() {
 # check os
 OS "ubuntu" "18"
 
