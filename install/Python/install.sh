47fb408c6f4295105dc353d186338c39f2db29e2
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-16 10:00:50 +0800

    Update install.sh

--- install/Python/install.sh
+++ install/Python/install.sh
@@ -8,36 +8,9 @@
 #
 
 source /etc/profile
+source <(curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/tool/common.sh)
 
-INFO() {
-printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
-printf -- "%s" "$1"
-printf "\n"
-}
-
-YELLOW() {
-printf -- "\033[44;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
-printf -- "\033[33m%s\033[0m" "$1"
-printf "\n"
-}
-
-ERROR() {
-printf -- "\033[41;37m%s\033[0m " "[$(date "+%Y-%m-%d %H:%M:%S")]"
-printf -- "%s" "$1"
-printf "\n"
-exit 1
-}
-
-EXEC() {
-local cmd="$1"
-INFO "${cmd}"
-eval ${cmd} 1> /dev/null
-if [ $? -ne 0 ]; then
-ERROR "Execution command (${cmd}) failed, please check it and try again."
-fi
-}
-
-function main() {
+main() {
 # environments
 version="${1}-dev"
 pythonCmd="python${1}"
