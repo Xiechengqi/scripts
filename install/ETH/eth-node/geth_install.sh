8d73a3311b2559a024f684bfdb8c00da25ec0b55
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-16 19:36:23 +0800

    Create geth_install.sh

---
+++ install/ETH/eth-node/geth_install.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+#
+# xiechengqi
+# 2021/08.03
+# https://github.com/ethereum/go-ethereum https://geth.ethereum.org/
+# Ubuntu 18.04
+# install ETH Node Geth
+#
+
+source /etc/profile
+BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+source <(curl -SsL $BASEURL/tool/common.sh)
+
+main()
+# check os
+OS "ubuntu" "18" || OS "ubuntu" "20"
+
+# environments
+serviceName="eth-node"
+version="1.10.5-33ca98ec"
+installPath="/data/ETH/${serviceName}-${version}"
+downloadUrl="https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-${version}.tar.gz"
+wsPort="8544"
+rpcPort="8545"
+
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running ..." && return 0
+
+# check install path
+EXEC "rm -rf $installPath $(dirname $installPath)/${serviceName}"
+EXEC "mkdir -p $installPath/{bin,data,logs}"
+
+# download tarball
+EXEC "curl -SsL $downloadUrl | tar zx --strip-components 1 -C $installPath"
+
+# register bin
+EXEC "mv $installPath/geth $installPath/bin"
+EXEC "ln -fs $installPath/bin/* /usr/local/bin"
+EXEC "geth version" && geth version
+
+# create start.sh
+pubIp=`curl -4 ip.sb`    # get vm public ip
+[ "$1" = "mainnet" ] && options="" || options="--rinkeby"
+cat > $installPath/start.sh << EOF
+#!/usr/bin/env bash
+source /etc/profile
+
+timestamp=\$(date +%Y%m%d%H%M%S)
+touch $installPath/logs/\${timestamp}.log && ln -fs $installPath/logs/\${timestamp}.log $installPath/logs/latest.log
+geth --nat=extip:$pubIp --http --http.addr 0.0.0.0 --ws --ws.addr 0.0.0.0 --ws.port $wsPort --datadir $installPath/data --http.vhosts=* $options &> $installPath/logs/latest.log
+EOF
+EXEC "chmod +x $installPath/start.sh"
+
+# register service
+cat > $installPath/${serviceName}.service << EOF
+[Unit]
+Description=Official Go implementation of the Ethereum protocol
+Documentation=https://github.com/ethereum/go-ethereum
+After=network.target
+
+[Service]
+User=root
+Group=root
+ExecStart=/bin/bash $installPath/start.sh
+ExecStop=/bin/kill -s QUIT \$MAINPID
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+EXEC "rm -f /lib/systemd/system/${serviceName}.service"
+EXEC "ln -fs $installPath/${serviceName}.service /lib/systemd/system/${serviceName}.service"
+
+# change softlink
+EXEC "ln -fs $installPath $(dirname $installPath)/$serviceName"
+
+# start
+EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
+EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
+
+# info
+YELLOW "${serviceName} version: $version"
+YELLOW "chain: $chainId"
+YELLOW "data: $installPath/data"
+YELLOW "log: tail -f $installPath/logs/latest.log"
+YELLOW "connection cmd: geth attach http://${pubIp}:${rpcPort}"
+YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] $serviceName"
+}
+
+main $@
