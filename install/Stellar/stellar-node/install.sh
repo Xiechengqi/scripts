ca16b6499dff453a7dfa8ac8aba85609d12d8a7f
Xiechengqi <Xiechengqi01@gmail.com>
2021-08-20 11:06:19 +0800

    Update install.sh

--- install/Stellar/stellar-node/install.sh
+++ install/Stellar/stellar-node/install.sh
@@ -36,11 +36,15 @@ curl -SsL $dockerUrl | bash
 
 # docker run service
 [ "$chainId" = "mainnet" ] && options="--pubnet" || options="--testnet"
+INFO "init postgresql and set postgresql password, then use CTRL C to break out this init container ..."
+EXEC "docker run --rm -it -p $port:8000 -v $installPath/data:/opt/stellar --name ${serviceName} stellar/quickstart $options"
+EXEC "sed 's/PER_HOUR_RATE_LIMIT=.*/PER_HOUR_RATE_LIMIT=72000000/' horizon.env"
 EXEC "docker run -itd --restart=always -p $port:8000 -v $installPath/data:/opt/stellar --name ${serviceName} stellar/quickstart $options"
 
 # check
 EXEC "sleep 5"
-EXEC "docker ps | grep ${serviceName}"
+INFO "docker ps | grep ${serviceName}" && docker ps | grep ${serviceName}
+INFO "curl 127.0.0.1:$port | grep core_latest_ledger" && curl 127.0.0.1:$port | grep core_latest_ledger
 
 # info
 YELLOW "port: $port"
