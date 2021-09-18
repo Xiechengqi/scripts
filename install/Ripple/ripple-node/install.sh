ecdb16b1cbe0f8640cfe8cf6d9a58db837be1675
Xiechengqi <Xiechengqi01@gmail.com>
2021-09-18 17:01:53 +0800

    Update install.sh

--- install/Ripple/ripple-node/install.sh
+++ install/Ripple/ripple-node/install.sh
@@ -30,51 +30,83 @@ systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName is running
 
 # check install path
 EXEC "rm -rf $installPath $(dirname $installPath)/${serviceName}"
-EXEC "mkdir -p $installPath"
+EXEC "mkdir -p $installPath/{conf,data,logs}"
 
 # install
 EXEC "apt update && apt install -y apt-transport-https ca-certificates wget gnupg"
 EXEC "wget -q -O - 'https://repos.ripple.com/repos/api/gpg/key/public' |  apt-key add -"
 EXEC "apt-key finger"
-EXEC "echo 'deb https://repos.ripple.com/repos/rippled-deb bionic stable' | tee -a /etc/apt/sources.list.d/ripple.list"
-EXEC "apt update && apt install --reinstall -y rippled"
+EXEC "echo 'deb https://repos.ripple.com/repos/rippled-deb bionic stable' > /etc/apt/sources.list.d/ripple.list"
+EXEC "apt update && apt install --reinstall -y rippled" 
+EXEC "cp -rf /opt/ripple/bin $installPath/bin && ln -fs $installPath/bin/* /usr/local/bin"
 
-# mv and ln conf bin data logs service
-EXEC "mv /opt/ripple/bin $installPath/bin && ln -fs $installPath/bin /opt/ripple/bin"
-EXEC "mv /opt/ripple/etc $installPath/conf && ln -fs $installPath/conf /opt/ripple/etc"
-EXEC "mv /var/lib/rippled/db $installPath/data && ln -fs $installPath/data /var/lib/rippled/db"
-EXEC "mv /var/log/rippled $installPath/logs && ln -fs $installPath/logs /var/log/rippled"
-EXEC "systemctl disable --now rippled && rm -f /lib/systemd/system/rippled.service"
-
-# register bin
-EXEC "ln -fs $installPath/bin/* /usr/local/bin"
+# uninstall default apt installed rippled
+EXEC "systemctl stop rippled && apt remove -y rippled && apt purge -y rippled"
 
 # conf
 ## https://xrpl.org/connect-your-rippled-to-the-xrp-test-net.html
+cat > $installPath/conf/rippled.cfg << EOF
+[server]
+port_rpc_admin_local
+port_peer
+port_ws_admin_local
+[port_rpc_admin_local]
+port = 5005
+ip = 127.0.0.1
+admin = 127.0.0.1
+protocol = http
+[port_peer]
+port = 51235
+ip = 0.0.0.0
+protocol = peer
+[port_ws_admin_local]
+port = 6006
+ip = 127.0.0.1
+admin = 127.0.0.1
+protocol = ws
+[node_size]
+medium
+[node_db]
+type=NuDB
+path=$installPath/data/nudb
+online_delete=512
+advisory_delete=0
+[database_path]
+$installPath/data
+[debug_logfile]
+$installPath/logs/debug.log
+[sntp_servers]
+time.windows.com
+time.apple.com
+time.nist.gov
+pool.ntp.org
+[validators_file]
+validators.txt
+[rpc_startup]
+{ "command": "log_level", "severity": "warning" }
+[ssl_verify]
+1
+EOF
+
 if [ "$chainId" = "mainnet" ]
 then
-
-cat > /opt/ripple/etc/validators.txt << EOF
+cat > $installPath/conf/validators.txt << EOF
 [validator_list_sites]
 https://vl.ripple.com
 [validator_list_keys]
 ED2677ABFFD1B33AC6FBC3062B71F1E8397C1505E1C42C64D11AD1B28FF73F4734
 EOF
-
 else
-
-cat > /opt/ripple/etc/validators.txt << EOF
+cat > $installPath/conf/validators.txt << EOF
 [validator_list_sites]
 https://vl.altnet.rippletest.net
 [validator_list_keys]
 ED264807102805220DA0F312E71FC2C69E1552C9C5790F6C25E3729DEB573D5860
 EOF
-
-cat >> /opt/ripple/etc/rippled.cfg << EOF
+cat >> $installPath/conf/rippled.cfg << EOF
 [ips]
 r.altnet.rippletest.net 51235
 EOF
-
 fi
 
 # register service
