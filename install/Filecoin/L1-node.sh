014e0a643cb586ba1d6bb97d9d054f996cb26415
Xiechengqi <Xiechengqi01@gmail.com>
2022-11-02 16:25:00 +0800

    Update L1-node.sh

--- install/Filecoin/L1-node.sh
+++ install/Filecoin/L1-node.sh
@@ -31,6 +31,7 @@ docker run --name saturn-node -it -d \
   -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL \
   --network host \
   --ulimit nofile=1000000 \
-  ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK
+  fullnode/filecoin-saturn-l1-node:$SATURN_NETWORK
+#  ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK
 INFO "docker ps | grep saturn-node"
 docker ps | grep saturn-node
