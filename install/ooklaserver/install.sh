8d5d8afac5a5d9b5fa55212a893bb02790ebe650
Xiechengqi <Xiechengqi01@gmail.com>
2022-11-25 21:47:51 +0800

    Create install.sh

---
+++ install/ooklaserver/install.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+#
+# xiechengqi
+# 2022/11/25
+# install ooklaserver
+#
+
+source /etc/profile
+BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+source <(curl -SsL $BASEURL/tool/common.sh)
+
+main() {
+# check os
+osInfo=`get_os` && INFO "current os: $osInfo"
+! echo "$osInfo" | grep -E 'centos7|ubuntu18|ubuntu20' &> /dev/null && ERROR "You could only install on os: centos7、ubuntu18、ubuntu20"
+
+# environments
+serviceName="ooklaserver"
+installPath="/data/${serviceName}"
+downloadUrl="https://install.speedtest.net/ooklaserver/stable/OoklaServer.tgz"
+
+# check service
+systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName has been installed ..." && return 0
+
+# check install path
+EXEC "rm -rf ${installPath}"
+EXEC "mkdir -p $installPath/{src,bin,logs}"
+
+# download tarball
+EXEC "cd ${installPath}/src"
+EXEC "curl -sSL ${downloadUrl} -o OoklaServer.tgz"
+EXEC "tar xvf OoklaServer.tgz"
+EXEC "mv OoklaServer-linux64.tgz ${installPath}"
+EXEC "cd ${installPath}"
+EXEC "tar xvf OoklaServer-linux64.tgz"
+
+# register bin
+EXEC "ln -fs ${installPath}/OoklaServer /usr/local/bin/OoklaServer"
+
+# create start.sh
+cat > $installPath/start.sh << EOF
+#!/usr/bin/env bash
+source /etc/profile
+
+timestamp=\$(date +%Y%m%d-%H%M%S)
+touch $installPath/logs/\${timestamp}.log && ln -fs $installPath/logs/\${timestamp}.log $installPath/logs/latest.log
+
+cd ${installPath}
+OoklaServer &> $installPath/logs/latest.log
+EOF
+EXEC "chmod +x $installPath/start.sh"
+
+# register service
+EXEC "rm -f /lib/systemd/system/${serviceName}.service"
+cat > /lib/systemd/system/${serviceName}.service << EOF
+[Unit]
+Description=OoklaServer
+After=network.target
+
+[Service]
+User=root
+Group=root
+ExecStart=/bin/bash $installPath/start.sh
+ExecStop=/bin/kill -s QUIT \$MAINPID
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start
+EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
+EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
+
+# info
+YELLOW "install path: $installPath"
+YELLOW "tail log cmd: tail -f $installPath/logs/latest.log"
+YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] $serviceName"
+}
+
+main $@
