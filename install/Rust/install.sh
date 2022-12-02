2db64e54d3fefad5d78bddc02fc8e9a6bd671d4b
Xiechengqi <Xiechengqi01@gmail.com>
2022-12-02 15:16:04 +0800

    Update install.sh

--- install/Rust/install.sh
+++ install/Rust/install.sh
@@ -1,1 +1,4 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#!/usr/bin/env bash
+
+source $HOME/.cargo/env
+cargo version || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
