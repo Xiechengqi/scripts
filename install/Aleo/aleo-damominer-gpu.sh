6f364a9ecbeaf218c3f648fb1d7993cf026d298e
Xiechengqi <Xiechengqi01@gmail.com>
2022-12-13 17:19:42 +0800

    Update aleo-damominer-gpu.sh

--- install/Aleo/aleo-damominer-gpu.sh
+++ install/Aleo/aleo-damominer-gpu.sh
@@ -14,19 +14,20 @@ function install() {
 
 local address=$1
 local gpu_num=$2
+local cpu_list=$3
 
 installPath="/scratch/aleo-gpu/${gpu_num}"
 EXEC "rm -rf ${installPath}"
 EXEC "mkdir -p ${installPath}/{bin,logs}"
-EXEC "curl -SsL ${downloadUrl} -o ${installPath}/bin/damominer"
-EXEC "chmod +x ${installPath}/bin/damominer"
+EXEC "curl -SsL ${downloadUrl} -o ${installPath}/bin/${binaryName}"
+EXEC "chmod +x ${installPath}/bin/${binaryName}"
 
 INFO "create /etc/supervisor/conf.d/aleo-gpu-${gpu_num}.conf ..."
 cat > /etc/supervisor/conf.d/aleo-gpu-${gpu_num}.conf << EOF
 [program:aleo-gpu-${gpu_num}]
-directory=/scratch/aleo-gpu/${gpu_num}
-command=/scratch/aleo-gpu/${gpu_num}/bin/damominer --address ${address} --proxy ${aleo_proxy} --gpu ${gpu_num}
-stdout_logfile=/scratch/aleo-gpu/${gpu_num}/logs/latest.log
+directory=${installPath}
+command=taskset -c ${cpu_list} ${installPath}/bin/${binaryName} --address ${address} --proxy ${aleo_proxy} --gpu ${gpu_num}
+stdout_logfile=${installPath}/logs/latest.log
 redirect_stderr=true
 EOF
 
@@ -40,9 +41,11 @@ osInfo=`get_os` && INFO "current os: $osInfo"
 # aleo address
 aleo_address=${1-"aleo1wfz88rr2wnuk65pxzgk8ewlzr2vhltzq2ggev3dq60nrd2e9lggqunt6cg"}
 # aleo proxy url
-aleo_proxy="aleo1.damominer.hk:9090"
+aleo_proxy="aleo3.damominer.hk:9090"
+# binary name
+binaryName="damominer"
 # download url
-downloadUrl="http://10.19.5.20:5000/aleo/bin/damominer"
+downloadUrl="http://10.19.5.20:5000/aleo/bin/${binaryName}"
 
 # check service
 cd /etc/supervisor &> /dev/null && supervisorctl status | grep aleo &> /dev/null && YELLOW "aleo is running ..." && return 0
@@ -57,12 +60,25 @@ gpu_sum=$(nvidia-smi -L | grep -E '^GPU' | wc -l)
 # install supervisor
 ! systemctl is-active supervisor &> /dev/null && curl -SsL https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Supervisor/install.sh | sudo bash
 
+# bind cpu
+gpu_sum=$(nvidia-smi -L | grep -E '^GPU' | wc -l)
+cpu_cores=$(lscpu | grep '^CPU(s):' | awk '{print $2}')
+physical_cores=$(( cpu_cores / 2 ))
+append=$(( physical_cores % gpu_sum ))
+span=$(( physical_cores / gpu_sum ))
+
 # install aleo-gpu
 for num in $(seq 0 `expr ${gpu_sum} - 1`)
 do
 
-INFO "install ${aleo_address} ${num}"
-install ${aleo_address} ${num}
+cpu_list="$((num * span))-$(((num+1) * span - 1)),$((num * span + physical_cores))-$(((num+1) * span + physical_cores - 1))"
+if [[ $append -gt 0 ]]; then
+cpu_list+=",$(( physical_cores - append )),$(( cpu_cores - append ))"
+append=$(( append - 1 ))
+fi
+
+INFO "install ${aleo_address} ${num} ${cpu_list}"
+install ${aleo_address} ${num} ${cpu_list}
 
 done
 
