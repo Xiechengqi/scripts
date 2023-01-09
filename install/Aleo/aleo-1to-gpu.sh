da96f35e361fb74838b17fae7b5193b260620598
Xiechengqi <Xiechengqi01@gmail.com>
2023-01-09 11:32:40 +0800

    Update aleo-1to-gpu.sh

--- install/Aleo/aleo-1to-gpu.sh
+++ install/Aleo/aleo-1to-gpu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# 2023/01/06
+# 2023/01/09
 # xiechengqi
 # https://mp.weixin.qq.com/s/Bc9OQmrQIpYK3U902UnLWA
 #
@@ -14,23 +14,20 @@ function install() {
 
 local aleo_address=$1
 local proxy=$2
-local gpu_num=$3
-local cpu_list=$4
 
-installPath="/scratch/aleo-gpu/${gpu_num}"
+installPath="/scratch/aleo-gpu"
 EXEC "rm -rf ${installPath}"
 EXEC "mkdir -p ${installPath}/{bin,logs}"
 EXEC "curl -SsL ${downloadUrl} -o ${installPath}/bin/${binaryName}"
 EXEC "chmod +x ${installPath}/bin/${binaryName}"
 
-INFO "create /etc/supervisor/conf.d/aleo-gpu-${gpu_num}.conf ..."
-cat > /etc/supervisor/conf.d/aleo-gpu-${gpu_num}.conf << EOF
-[program:aleo-gpu-${gpu_num}]
-directory=/scratch/aleo-gpu/${gpu_num}
-command=taskset -c ${cpu_list} /scratch/aleo-gpu/${gpu_num}/bin/${binaryName} --address ${aleo_address} --solo --ws ${proxy}
-stdout_logfile=/scratch/aleo-gpu/${gpu_num}/logs/latest.log
+INFO "create /etc/supervisor/conf.d/aleo-gpu.conf ..."
+cat > /etc/supervisor/conf.d/aleo-gpu.conf << EOF
+[program:aleo-gpu]
+directory=${installPath}
+command=${installPath}/bin/${binaryName} --address ${aleo_address} --solo --ws ${proxy}
+stdout_logfile=${installPath}/logs/latest.log
 redirect_stderr=true
-environment=CUDA_VISIBLE_DEVICES=${gpu_num}
 EOF
 
 }
@@ -43,7 +40,7 @@ osInfo=`get_os` && INFO "current os: $osInfo"
 # aleo address
 aleo_address=${1-"aleo1wfz88rr2wnuk65pxzgk8ewlzr2vhltzq2ggev3dq60nrd2e9lggqunt6cg"}
 # 1to proxy
-proxy=${2-"wss://shadow.aleo1.to:32443"}
+proxy=${2-"ws://pool.aleo1.to:32000"}
 # BASEURL="http://cn.1to.sh/builds/aleo/partners/miner-9q57lnd4"
 BASEURL="http://10.19.5.20:5000/aleo/bin"
 # binary name
@@ -70,27 +67,9 @@ then
 curl -SsL https://raw.githubusercontent.com/Xiechengqi/scripts/master/install/Supervisor/install.sh | sudo bash || exit 1
 fi
 
-# bind cpu
-gpu_sum=$(nvidia-smi -L | grep -E '^GPU' | wc -l)
-cpu_cores=$(lscpu | grep '^CPU(s):' | awk '{print $2}')
-physical_cores=$(( cpu_cores / 2 ))
-append=$(( physical_cores % gpu_sum ))
-span=$(( physical_cores / gpu_sum ))
-
 # install aleo-gpu
-for num in $(seq 0 `expr ${gpu_sum} - 1`)
-do
-
-cpu_list="$((num * span))-$(((num+1) * span - 1)),$((num * span + physical_cores))-$(((num+1) * span + physical_cores - 1))"
-if [[ $append -gt 0 ]]; then
-cpu_list+=",$(( physical_cores - append )),$(( cpu_cores - append ))"
-append=$(( append - 1 ))
-fi
-
-INFO "install ${aleo_address} ${proxy} ${num} ${cpu_list}"
-install ${aleo_address} ${proxy} ${num} ${cpu_list}
-
-done
+INFO "install ${aleo_address} ${proxy}"
+install ${aleo_address} ${proxy}
 
 # start
 EXEC "cd /etc/supervisor"
