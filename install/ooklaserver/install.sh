bec54395cc62acba6dbfd7f4475d782909a57320
Xiechengqi <Xiechengqi01@gmail.com>
2023-01-25 16:11:20 +0800

    Update install.sh

--- install/ooklaserver/install.sh
+++ install/ooklaserver/install.sh
@@ -7,7 +7,8 @@
 #
 
 source /etc/profile
-BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+# BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+BASEURL="https://raw.githubusercontent.com/Xiechengqi/scripts/master"
 source <(curl -SsL $BASEURL/tool/common.sh)
 
 main() {
