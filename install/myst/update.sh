eda9ca4f50f04a3b19a9994a72ee76a62fc7979f
Xiechengqi <Xiechengqi01@gmail.com>
2023-01-27 22:32:36 +0800

    Create update.sh

---
+++ install/myst/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /etc/profile
+
+out=$(docker pull mysteriumnetwork/myst:latest)
+if [[ $out != *"up to date"* ]]; then
+
+docker pull mysteriumnetwork/myst:latest || true
+docker stop myst || true
+docker rm -f myst || true
+docker run -itd --cap-add NET_ADMIN -p 4449:4449 -v /data/myst/data:/var/lib/mysterium-node --restart unless-stopped --name myst mysteriumnetwork/myst:latest service --agreed-terms-and-conditions
+docker image prune -f
+echo "Updated to latest version successfully!"
+
+else
+
+echo "Node up to date"
+
+fi
