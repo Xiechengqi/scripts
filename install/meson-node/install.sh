129f0e1eb9679f4d56a153041611d1b2f1c257be
Xiechengqi <Xiechengqi01@gmail.com>
2023-01-28 15:14:01 +0800

    Update install.sh

--- install/meson-node/install.sh
+++ install/meson-node/install.sh
@@ -20,7 +20,7 @@ export CACHE_SIZE="30"
 # environment
 serviceName="meson_cdn"
 version=${1-"3.1.19"}
-installPath="/data/${serviceName}_${version}"
+installPath="/data/${serviceName}"
 downloadUrl="https://staticassets.meson.network/public/meson_cdn/v${version}/meson_cdn-linux-amd64.tar.gz"
 binaryName="meson_cdn"
 
@@ -37,7 +37,7 @@ EXEC "curl -SsL ${downloadUrl} | tar zx --strip-components 1 -C ${installPath}"
 # start
 EXEC "cd ${installPath}"
 EXEC "./service install ${serviceName}"
-EXEC "./${serviceName} config set --token=${MESON_TOKEN} --https_port=${HTTPS_PORT} --cache.size=${CACHE_SIZE}"
+EXEC "./${binaryName} config set --token=${MESON_TOKEN} --https_port=${HTTPS_PORT} --cache.size=${CACHE_SIZE}"
 EXEC "./service start ${serviceName}"
 INFO "./service status ${serviceName}" && ./service status ${serviceName}
 INFO "./${binaryName} config show" && ./${binaryName} config show
