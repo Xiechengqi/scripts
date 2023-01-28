457f5143bfa6ee061b2801440d1a686b1f4bb721
Xiechengqi <Xiechengqi01@gmail.com>
2023-01-28 14:26:41 +0800

    Create install.sh

---
+++ install/meson-node/install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#
+# 2023/01/28
+# xiechengqi
+# install meson node
+# https://dashboard.meson.network/user_node
+#
+
+source /etc/profile
+BASEURL="https://raw.githubusercontent.com/Xiechengqi/scripts/master"
+source <(curl -SsL $BASEURL/tool/common.sh)
+
+main() {
+
+export MESON_TOKEN="ndofiwxlyoaqyiaicyydvtqh"
+export HTTPS_PORT="54321"
+export CACHE_SIZE="30"
+
+# environment
+serviceName="meson_cdn"
+version=${1-"3.1.19"}
+installPath="/data/${serviceName}_${version}"
+downloadUrl="https://staticassets.meson.network/public/meson_cdn/v${version}/meson_cdn-linux-amd64.tar.gz"
+binaryName="meson_cdn"
+
+# check node
+which ${binaryName} &> /dev/null && YELLOW "${serviceName} has been installed ..." && return 0
+
+# check install path
+EXEC "rm -rf ${installPath} $(dirname ${installPath})/${serviceName}"
+EXEC "mkdir -p ${installPath}/logs"
+
+# download tarball
+EXEC "curl -SsL ${downloadUrl} | tar zx --strip-components 1 -C ${installPath}"
+
+# start
+EXEC "cd ${installPath}"
+EXEC "./service install ${serviceName}"
+EXEC "./${serviceName} config set --token=${MESON_TOKEN} --https_port=${HTTPS_PORT} --cache.size=${CACHE_SIZE}"
+EXEC "./service start ${serviceName}"
+INFO "./service status ${serviceName}" && ./service status ${serviceName}
+INFO "./${binaryName} config show" && ./${binaryName} config show
+
+}
+
+main $@
