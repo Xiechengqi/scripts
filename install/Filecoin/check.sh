a70f0ed1137f90da1de281ba0d896add9432f394
Xiechengqi <Xiechengqi01@gmail.com>
2023-01-29 23:09:19 +0800

    Create check.sh

---
+++ install/Filecoin/check.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+trap "_clean" EXIT
+
+_clean() {
+
+cd /tmp/ && rm -f $$_*
+
+}
+
+main() {
+
+curl -SsL 'https://orchestrator.strn.pl/nodes/local' > /tmp/$$_local 2>/dev/null
+ip=$(curl -SsL ip.sb)
+cat /tmp/$$_local | grep "\"${ip}\"" &> /dev/null && echo "${ip} ... online" || echo "${ip} ... offline"
+
+}
+
+main $@
