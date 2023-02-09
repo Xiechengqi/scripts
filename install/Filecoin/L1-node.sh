6baaa2f02f68a3a95cfa12bf60bdfbffef6e0e93
Xiechengqi <Xiechengqi01@gmail.com>
2023-02-09 17:22:57 +0800

    Update L1-node.sh

--- install/Filecoin/L1-node.sh
+++ install/Filecoin/L1-node.sh
@@ -61,6 +61,7 @@ EOF
 INFO "docker run --name saturn-node -it -d --restart=unless-stopped -v $SATURN_HOME/shared:/usr/src/app/shared -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL --network host --ulimit nofile=1000000 ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK"
 docker run --name saturn-node -it -d \
   --restart=unless-stopped \
+  -v /var/lib/lxcfs/proc/meminfo:/proc/meminfo:rw \
   -v $SATURN_HOME/shared:/usr/src/app/shared \
   -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS \
   -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL \
