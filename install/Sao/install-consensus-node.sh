6d32cc4facdfd80faf9942ea408095a8cf6f9839
Xiechengqi <Xiechengqi01@gmail.com>
2023-02-22 15:57:48 +0800

    Create install-consensus-node.sh

---
+++ install/Sao/install-consensus-node.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+source /etc/profile
+BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+source <(curl -SsL $BASEURL/tool/common.sh)
+
+main() {
+# check os
+osInfo=`get_os` && INFO "current os: $osInfo"
+! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20"
+
+# environments
+serviceName="saod"
+installPath="/data/${serviceName}"
+binaryName="saod"
+binaryDownloadUrl="http://10.19.10.244:5000/sao/${binaryName}"
+configDownloadUrl="http://10.19.10.244:5000/sao/config.tar.gz"
+
+# check service
+systemctl is-active ${serviceName} &> /dev/null && YELLOW "${serviceName} is running ..." && return 0
+
+# check install path
+EXEC "rm -rf ${installPath}"
+EXEC "mkdir -p ${installPath}/{bin,home,data,logs}"
+
+# download binary
+EXEC "curl -SsL ${binaryDownloadUrl} -o ${installPath}/bin/${binaryname}"
+EXEC "chmod +x ${installPath}/bin/${binaryname}"
+EXEC "ln -fs ${installPath}/bin/${binaryname} /usr/local/bin/${binaryname}"
+INFO "saod version" && saod version
+
+# download config
+EXEC "curl -SsL ${configDownloadUrl} | tar zx -C ${installPath}/home"
+
+# create start.sh
+cat > ${installPath}/start.sh << EOF
+#!/usr/bin/env bash
+source /etc/profile
+
+monikerName="\$(hostname)"
+installPath="${installPath}"
+timestamp=\$(date +%Y%m%d-%H%M%S)
+touch \$installPath/logs/\${timestamp}.log && ln -fs \$installPath/logs/\${timestamp}.log \$installPath/logs/latest.log
+
+\${installPath}/bin/saod start --db_dir \${installPath}/data --home \${installPath}/home --moniker \${monikerName} &> \${installPath}/logs/latest.log
+EOF
+EXEC "chmod +x ${installPath}/start.sh"
+
+# register service
+cat > ${installPath}/${serviceName}.service << EOF
+[Unit]
+Description=SAO Consensus Node
+Documentation=https://github.com/SaoNetwork/sao-consensus
+After=network.target
+
+[Service]
+User=root
+Group=root
+ExecStart=/bin/bash $installPath/start.sh
+ExecStop=/bin/kill -s QUIT \$MAINPID
+Restart=always
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+EXEC "rm -f /lib/systemd/system/${serviceName}.service"
+EXEC "ln -fs ${installPath}/${serviceName}.service /lib/systemd/system/${serviceName}.service"
+
+# start
+EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
+EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
+
+# INFO
+YELLOW "${serviceName} version: ${version}"
+YELLOW "log: tail -f $installPath/logs/latest.log"
+YELLOW "check cmd: saod status"
+YELLOW "control cmd: systemctl [stop|start|restart|reload] $serviceName"
+}
+
+main $@
