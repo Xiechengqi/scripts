7383100292ed7ba4e44f471a830ff2bc9c5af266
Xiechengqi <Xiechengqi01@gmail.com>
2023-02-23 16:36:49 +0800

    Update install-faucet.sh

--- install/Sao/install-faucet.sh
+++ install/Sao/install-faucet.sh
@@ -33,9 +33,9 @@ cat > ${installPath}/start.sh << EOF
 #!/usr/bin/env /bash
 source /etc/profile
 
-export FAUCET_FROM="${FAUCET_FROM}"
-export chainAddress="${chainAddress}"
+export chainAddress="http://127.0.0.1:26657"
 export FAUCET_SECRET_KEY="${FAUCET_SECRET_KEY}"
+export FAUCET_FROM="${FAUCET_FROM}"
 
 installPath="/data/sao-faucet"
 
