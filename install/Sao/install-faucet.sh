287e30a9f3a98e11dd0f0d343ec2297d38ab0212
Xiechengqi <Xiechengqi01@gmail.com>
2023-02-23 10:07:44 +0800

    Update install-faucet.sh

--- install/Sao/install-faucet.sh
+++ install/Sao/install-faucet.sh
@@ -10,7 +10,7 @@ osInfo=`get_os` && INFO "current os: $osInfo"
 ! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20"
 
 # environments
-serviceName="saod"
+serviceName="sao-faucet"
 installPath="/data/${serviceName}"
 binaryName="faucet-api"
 binaryDownloadUrl="http://205.204.75.250:5000/sao/${binaryName}"
@@ -20,28 +20,29 @@ systemctl is-active ${serviceName} &> /dev/null && YELLOW "${serviceName} is run
 
 # check install path
 EXEC "rm -rf ${installPath}"
-EXEC "mkdir -p ${installPath}/{bin,home,data,logs}"
+EXEC "mkdir -p ${installPath}/{bin,logs}"
 
 # download binary
 EXEC "curl -SsL ${binaryDownloadUrl} -o ${installPath}/bin/${binaryName}"
 EXEC "chmod +x ${installPath}/bin/${binaryName}"
 EXEC "ln -fs ${installPath}/bin/${binaryName} /usr/local/bin/${binaryName}"
-INFO "saod version" && saod version
-
-# download config
-EXEC "curl -SsL ${configDownloadUrl} | tar zx -C ${installPath}/home"
+INFO "${binaryDownloadUrl} version" && ${binaryDownloadUrl} version
 
 # create start.sh
 cat > ${installPath}/start.sh << EOF
-#!/usr/bin/env bash
+#!/usr/bin/env /bash
 source /etc/profile
 
-monikerName="\$(hostname)"
-installPath="${installPath}"
-timestamp=\$(date +%Y%m%d-%H%M%S)
-touch \$installPath/logs/\${timestamp}.log && ln -fs \$installPath/logs/\${timestamp}.log \$installPath/logs/latest.log
+export FAUCET_FROM="xxx"
+export chainAddress="http://127.0.0.1:26656"
+export FAUCET_SECRET_KEY="xxx"
 
-\${installPath}/bin/${binaryName} start --db_dir \${installPath}/data --home \${installPath}/home --moniker \${monikerName} &> \${installPath}/logs/latest.log
+installPath="/data/sao-faucet"
+
+timestamp=\$(date +%Y%m%d%H%M%S)
+touch \${installPath}/logs/\${timestamp}.log && ln -fs \${installPath}/logs/\${timestamp}.log \${installPath}/logs/latest.log
+
+\${installPath}/bin/faucet-api &> \${installPath}/logs/latest.log
 EOF
 EXEC "chmod +x ${installPath}/start.sh"
 
@@ -49,6 +50,7 @@ EXEC "chmod +x ${installPath}/start.sh"
 cat > ${installPath}/${serviceName}.service << EOF
 [Unit]
 Description=SAO Faucet
+Documentation=https://github.com/SaoNetwork/faucet-api
 After=network.target
 
 [Service]
@@ -70,10 +72,8 @@ EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl star
 EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
 
 # INFO
-YELLOW "${serviceName} version: ${version}"
 YELLOW "log: tail -f $installPath/logs/latest.log"
-YELLOW "check cmd: saod status"
-YELLOW "control cmd: systemctl [stop|start|restart|reload] $serviceName"
+YELLOW "control cmd: systemctl [stop|start|restart|reload] ${serviceName}"
 }
 
 main $@
