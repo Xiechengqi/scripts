4df87587bdedeb43316d4142945bf1a1fd14fa46
Xiechengqi <Xiechengqi01@gmail.com>
2023-04-18 16:55:23 +0800

    Create stake.sh

---
+++ install/Sao/stake.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#
+# 2023/04/18
+# xiechengqi
+# sao stake
+#
+
+saod tx staking create-validator \
+  --amount=1000000000sao \
+  --pubkey=$(saod tendermint show-validator) \
+  --moniker="$(hostname)" \
+  --commission-rate="0.10" \
+  --commission-max-rate="0.20" \
+  --commission-max-change-rate="0.01" \
+  --min-self-delegation="1000000" \
+  --gas="2000000" \
+  --gas-prices="0.0025sao" \
+  --from=$(saod keys list | grep address | head -1 | awk '{print $NF}')
