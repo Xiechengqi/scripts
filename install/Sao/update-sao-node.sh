215f776b62b2989fc4dfa694a7d6422daf690be6
Xiechengqi <Xiechengqi01@gmail.com>
2023-04-20 12:26:15 +0800

    Update and rename update-saonode.sh to update-sao-node.sh

--- install/Sao/update-sao-node.sh
+++ install/Sao/update-sao-node.sh
@@ -6,9 +6,10 @@ source <(curl -SsL $BASEURL/tool/common.sh)
 main() {
 
 serviceName="sao-node"
-installPath="/data/sao-node"
 binaryName="saonode"
-binaryDownloadUrl="http://203.23.128.181:50000/${binaryName}"
+binaryDownloadUrl=${1}
+[ ".${binaryDownloadUrl}" = "." ] && echo "Less Params binaryDownloadUrl" && exit 1
+installPath=${2-"/data/sao-node"}
 
 EXEC "systemctl stop ${serviceName}"
 EXEC "rm -f ${installPath}/bin/${binaryName}"
