42dad3e49cd5bcca177539d1816fa48151a9c9a7
Xiechengqi <Xiechengqi01@gmail.com>
2023-06-15 14:54:42 +0800

    Update get-local-validator-address.sh

--- install/Cosmos/get-local-validator-address.sh
+++ install/Cosmos/get-local-validator-address.sh
@@ -5,7 +5,7 @@ main() {
 export BINARY_NAME="$1"
 [ ".${BINARY_NAME}" = "." ] && echo "Usage: bash get-local-validator-address.sh [BINARY_NAME] [(ROOT_HOME)]" && exit 1
 export ROOT_HOME="$2"
-[ ".${ROOT_NAME}" = "." ] && echo "Usage: bash get-local-validator-address.sh [BINARY_NAME] [(ROOT_HOME)]" && exit 1
+[ ".${ROOT_HOME}" = "." ] && echo "Usage: bash get-local-validator-address.sh [BINARY_NAME] [(ROOT_HOME)]" && exit 1
 export validatorInfoPubKeyValue=$(${BINARY_NAME} --home ${ROOT_HOME} status | awk -F 'value":"' '{print $NF}' | awk -F '"' '{print $1}')
 export validatorOperatorAddress=$(${BINARY_NAME} --home ${ROOT_HOME} query staking validators | grep -A 20 "${validatorInfoPubKeyValue}" | grep 'operator_address:' | head -1 | awk '{print $NF}')
 export validatorWalletAddress=""
