17226da2dd5d38e10c7aa9d7a61197de69fe7933
Xiechengqi <Xiechengqi01@gmail.com>
2023-06-15 16:11:49 +0800

    Update download-sao-consensus-testnet1.sh

--- install/Sao/download-sao-consensus-testnet1.sh
+++ install/Sao/download-sao-consensus-testnet1.sh
@@ -5,3 +5,4 @@ installPath=$2
 version=$3
 curl -SsL ${binaryDownloadUrl} -o ${installPath}/bin/saod-${version}
 chmod +x ${installPath}/bin/saod-${version}
+${installPath}/bin/saod-${version} version
