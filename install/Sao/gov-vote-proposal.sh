524416bf5c002f33e1ba53de408958c87afeae45
Xiechengqi <Xiechengqi01@gmail.com>
2023-06-15 15:34:19 +0800

    Update gov-vote-proposal.sh

--- install/Sao/gov-vote-proposal.sh
+++ install/Sao/gov-vote-proposal.sh
@@ -13,7 +13,7 @@ export PROPOSAL_ID=${1}
 export PROPOSAL_VOTE=${2-"yes"}
 export SAO_HOME=${3-"/root/.sao"}
 ! saod --home ${SAO_HOME} query gov proposal ${PROPOSAL_ID} && echo "Can not find proposal ${PROPOSAL_ID}, exit ..." && exit 1
-export validatorAddress=$(curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/install/Cosmos/get-local-validator-address.sh | bash -s saod ${SAO_HOME} | awk '{print $NF}')
+export validatorAddress=$(curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/install/Cosmos/get-local-validator-address.sh | bash -s saod ${SAO_HOME} | awk -F ': ' '{print $NF}')
 [ ".${validatorAddress}" = "." ] && echo "Can not find validator address, exit ..." && exit 1
 echo "echo y | saod --home ${SAO_HOME} tx gov vote ${PROPOSAL_ID} ${PROPOSAL_VOTE} --from ${validatorAddress}"
 echo y | saod --home ${SAO_HOME} tx gov vote ${PROPOSAL_ID} ${PROPOSAL_VOTE} --from ${validatorAddress}
