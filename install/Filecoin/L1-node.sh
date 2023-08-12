23bd4b8bacbfa807cf3439c7901f6e8b324f9be8
Xiechengqi <Xiechengqi01@gmail.com>
2023-08-13 00:45:18 +0800

    Update L1-node.sh

--- install/Filecoin/L1-node.sh
+++ install/Filecoin/L1-node.sh
@@ -57,6 +57,8 @@ cat >> /etc/hosts << EOF
 127.0.0.1 speedtest.sinet.com.kh speedtest.sinet.com.kh.prod.hosts.ooklaserver.net
 # Server: HOSTMEIN IKE - Greece (id: 15410)
 127.0.0.1 ookla.hostmein.net ookla.hostmein.net.prod.hosts.ooklaserver.net
+# Server: Vodafone Egypt - Cairo (id: 34283)
+127.0.0.1 speedtest6.vodafone.com.eg speedtest6.vodafone.com.eg.prod.hosts.ooklaserver.net
 EOF
 INFO "docker run --name saturn-node -it -d --restart=unless-stopped -v $SATURN_HOME/shared:/usr/src/app/shared -e FIL_WALLET_ADDRESS=$FIL_WALLET_ADDRESS -e NODE_OPERATOR_EMAIL=$NODE_OPERATOR_EMAIL --network host --ulimit nofile=1000000 ghcr.io/filecoin-saturn/l1-node:$SATURN_NETWORK"
 docker run --name saturn-node -it -d \
