f6bb51e8acb22f8a668d42f30a340ae83536f165
Xiechengqi <Xiechengqi01@gmail.com>
2023-08-29 16:17:05 +0800

    Update get-local-validator-address.sh

--- install/Cosmos/get-local-validator-address.sh
+++ install/Cosmos/get-local-validator-address.sh
@@ -5,7 +5,7 @@ main() {
 export BINARY_NAME="$1"
 [ ".${BINARY_NAME}" = "." ] && echo "Usage: bash get-local-validator-address.sh [BINARY_NAME]" && exit 1
 
-validatorWalletAddress =$(${BINARY_NAME} debug addr $(curl -SsL http://localhost:26657/status | jq -r .result.node_info.id) | grep 'Bech32 Acc' | awk '{print $NF}')
+validatorWalletAddress=$(${BINARY_NAME} debug addr $(curl -SsL http://localhost:26657/status | jq -r .result.node_info.id) | grep 'Bech32 Acc' | awk '{print $NF}')
 echo "${BINARY_NAME} Local Validator Wallet Address: ${validatorWalletAddress}"
 
 }
