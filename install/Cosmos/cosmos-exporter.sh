6883f37d88b5e65c67ad7091dd50a1711be24a89
Xiechengqi <Xiechengqi01@gmail.com>
2023-09-08 10:50:39 +0800

    Update cosmos-exporter.sh

--- install/Cosmos/cosmos-exporter.sh
+++ install/Cosmos/cosmos-exporter.sh
@@ -241,7 +241,7 @@ done
 
 format_metric > /data/metric/.${chain_id} && mv /data/metric/.${chain_id} /data/metric/${chain_id}.prom
 
-! crontab -l | grep 'cosmos-exporter.sh' &> /dev/null && echo '*/1 * * * * bash '${installPath}'/cosmos-exporter.sh' && echo '*/1 * * * * curl -T /data/metric/*.prom http://8.222.210.19:5000/metrics/' echo '* */1 * * * curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/install/Cosmos/cosmos-exporter.sh -o '${installPath}'/cosmos-exporter.sh'
+! crontab -l | grep 'cosmos-exporter.sh' &> /dev/null && echo '*/1 * * * * bash '${installPath}'/cosmos-exporter.sh' && echo '00 00 * * * curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/install/Cosmos/cosmos-exporter.sh -o '${installPath}'/cosmos-exporter.sh'
 
 }
 
