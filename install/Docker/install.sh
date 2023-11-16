5a66d7d1718d512f7b6b155f5321f2c925f43a27
Xiechengqi <Xiechengqi01@gmail.com>
2023-11-16 17:22:46 +0800

    Update install.sh

--- install/Docker/install.sh
+++ install/Docker/install.sh
@@ -80,7 +80,7 @@ if [ "$countryCode" = "CN" ]
 then
 cat > /etc/docker/daemon.json << EOF
 {
-  "registry-mirrors": ["https://ykawlvjt.mirror.aliyuncs.com","https://docker.mirrors.ustc.edu.cn","https://reg-mirror.qiniu.com"],
+  "registry-mirrors": ["https://ykawlvjt.mirror.aliyuncs.com","https://docker.mirrors.sjtug.sjtu.edu.cn","https://docker.nju.edu.cn"],
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
   "log-opts": {
