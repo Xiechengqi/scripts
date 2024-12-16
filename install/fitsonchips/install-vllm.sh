fae987c47d04bc1b5f2fdaf725b1700f195cd9a2
Xiechengqi <Xiechengqi01@gmail.com>
2024-12-16 10:08:52 +0800

    Create install-vllm.sh

---
+++ install/fitsonchips/install-vllm.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+source /etc/profile
+BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+source <(curl -SsL $BASEURL/tool/common.sh)
+
+main() {
+
+export installPath="/data/fitsonchips-vllm"
+export IMAGE="squeezebits/fitsonchips-gpu-vllm:latest"
+mkdir -p ${installPath}
+
+export DEVICE_KEY=$1
+[ ".${DEVICE_KEY}" = "." ] && ERROR "bash [DEVICE_KEY] [DEVICE]"
+
+INFO "docker pull ${IMAGE}" && docker pull ${IMAGE} || ERROR "Pull image fail ..."
+
+export DEVICE=$2
+if [ ".${DEVICE}" = "." ]
+then
+cat > ${installPath}/docker-run.sh << EOF
+#!/usr/bin/env bash
+
+name="fitsonchips-vllm"
+docker rm -f \${name}
+docker run -itd \\
+--restart=unless-stopped \\
+--gpus all \\
+-e DEVICE_KEY=${DEVICE_KEY} \\
+--name \${name} \\
+${IMAGE} \\
+./start_worker.sh
+EOF
+eles
+cat > ${installPath}/docker-run.sh << EOF
+#!/usr/bin/env bash
+
+name="fitsonchips-vllm"
+docker run -itd \\
+--restart=unless-stopped \\
+--gpus '"device=0"' \\
+-e DEVICE_KEY=${DEVICE_KEY} \\
+--name \${name} \\
+${IMAGE} \\
+./start_worker.sh
+EOF
+fi
+EXEC "chmod +x ${installPath}/docker-run.sh"
+INFO "bash ${installPath}/docker-run.sh" && bash ${installPath}/docker-run.sh || ERROR "Run fail ..."
+
+}
+
+main $@
