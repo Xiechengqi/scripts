d2796635e086f5eca7ac68a21f1f400a28cd1dde
Xiechengqi <Xiechengqi01@gmail.com>
2025-02-14 22:10:04 +0800

    Create install.sh

---
+++ install/tailscaled/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#
+# xiechengqi
+# 2025/02/14
+# install tailscaled
+#
+
+# source /etc/profile
+BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
+source <(curl -SsL $BASEURL/tool/common.sh)
+
+main() {
+
+# check os
+osInfo=`get_os` && INFO "current os: $osInfo"
+! echo "$osInfo" | grep -E 'ubuntu20' &> /dev/null && ERROR "You could only install on os: centos7、ubuntu18、ubuntu20"
+
+# environments
+serviceName="tailscaled"
+
+# check servcie
+systemctl is-active ${serviceName} &> /dev/null && YELLOW "${serviceName} is running ..." && return 0
+
+cat > /etc/apt/sources.list.d/tailscale.list << EOF
+deb [signed-by=/usr/share/keyrings/tailscale-archive-keyring.gpg] https://pkgs.tailscale.com/stable/ubuntu focal main
+EOF
+
+EXEC "apt update"
+INFO "apt-get install -y tailscale tailscale-archive-keyring" && apt-get install -y tailscale tailscale-archive-keyring || exit 1
+EXEC "systemctl enable --now tailscaled"
+EXEC "systemctl status tailscaled"
+INFO "tailscale up" && tailscale up
+
+}
+
+main $@
