04d821a4928f0e4f4579ceb2d1160040ce647e90
Xiechengqi <Xiechengqi01@gmail.com>
2025-03-04 13:04:37 +0800

    Update install.sh

--- install/Golang/install.sh
+++ install/Golang/install.sh
@@ -14,7 +14,7 @@ source <(curl -SsL $BASEURL/tool/common.sh)
 main() {
 # check os
 osInfo=`get_os` && INFO "current os: $osInfo"
-! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20|centos7|centos8' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20、centos7、centos8"
+! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20|ubuntu22|centos7|centos8' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20、ubuntu22、centos7、centos8"
 
 # environments
 serviceName="golang"
