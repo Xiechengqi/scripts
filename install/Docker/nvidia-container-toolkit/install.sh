58f335107ffb311a75167a014789729bcfc7463e
Xiechengqi <Xiechengqi01@gmail.com>
2025-03-12 00:42:49 +0800

    Update install.sh

--- install/Docker/nvidia-container-toolkit/install.sh
+++ install/Docker/nvidia-container-toolkit/install.sh
@@ -15,13 +15,10 @@ main() {
 osInfo=`get_os` && INFO "current os: $osInfo"
 ! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20|ubuntu22' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20、ubuntu22"
 
-EXEC "curl -fsSL https://gitee.com/Xiechengqi/scripts/raw/master/install/Docker/nvidia-container-toolkit/nvidia-container-toolkit-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg"
-cat > /etc/apt/sources.list.d/nvidia-container-toolkit.list << EOF
-deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://nvidia.github.io/libnvidia-container/stable/deb/\$(ARCH) /
-EOF
-INFO "cat /etc/apt/sources.list.d/nvidia-container-toolkit.list" && cat /etc/apt/sources.list.d/nvidia-container-toolkit.list
-INFO "apt update" && apt update || exit 1
-INFO "apt install -y nvidia-container-toolkit" && apt install -y nvidia-container-toolkit
+EXEC "curl -fsSL https://gitee.com/Xiechengqi/scripts/raw/master/install/Docker/nvidia-container-toolkit/libnvidia-container1_1.17.5-1_amd64.deb -o /tmp/libnvidia-container1_1.17.5-1_amd64.deb && dpkg -i /tmp/libnvidia-container1_1.17.5-1_amd64.deb"
+EXEC "curl -fsSL https://gitee.com/Xiechengqi/scripts/raw/master/install/Docker/nvidia-container-toolkit/libnvidia-container-tools_1.17.5-1_amd64.deb -o /tmp/libnvidia-container-tools_1.17.5-1_amd64.deb && dpkg -i /tmp/libnvidia-container-tools_1.17.5-1_amd64.deb"
+EXEC "curl -fsSL https://gitee.com/Xiechengqi/scripts/raw/master/install/Docker/nvidia-container-toolkit/nvidia-container-toolkit-base_1.17.5-1_amd64.deb -o /tmp/nvidia-container-toolkit-base_1.17.5-1_amd64.deb && dpkg -i /tmp/nvidia-container-toolkit-base_1.17.5-1_amd64.deb"
+EXEC "curl -fsSL https://gitee.com/Xiechengqi/scripts/raw/master/install/Docker/nvidia-container-toolkit/nvidia-container-toolkit_1.17.5-1_amd64.deb -o /tmp/nvidia-container-toolkit_1.17.5-1_amd64.deb && dpkg -i /tmp/nvidia-container-toolkit_1.17.5-1_amd64.deb"
 # 使用 nvidia-ctk 命令修改 /etc/docker/daemon.json 文件，设置 Docker 默认使用 NVIDIA runtime
 EXEC "nvidia-ctk runtime configure --runtime=docker"
 INFO "cat /etc/docker/daemon.json" && cat /etc/docker/daemon.json
