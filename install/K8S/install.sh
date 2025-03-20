404f11064eac8000911a6f5f90890041733069b0
Xiechengqi <Xiechengqi01@gmail.com>
2025-03-20 12:53:09 +0800

    Update install.sh

--- install/K8S/install.sh
+++ install/K8S/install.sh
@@ -2,9 +2,11 @@
 
 #
 # xiechengqi
-# 2022/10/27
+# 2025/03/20
 # Ubuntu 18.04+
 # install kubeadm kubelet kubectl
+# China: https://mirrors.tuna.tsinghua.edu.cn/help/kubernetes/
+# Other: https://v1-28.docs.kubernetes.io/zh-cn/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 #
 
 source /etc/profile
@@ -13,40 +15,57 @@ source <(curl -SsL $BASEURL/tool/common.sh)
 
 _ubuntu() {
 
+INFO "apt remove kubectl kubelet kubeadm" && apt remove kubectl kubelet kubeadm
+EXEC "apt update"
+EXEC "apt install -y apt-transport-https ca-certificates curl gpg"
+EXEC "mkdir -p -m 755 /etc/apt/keyrings"
 EXEC "rm -f /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
 EXEC "curl -fsSL https://pkgs.k8s.io/core:/stable:/${version}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
+if [ "${countryCode}" = "China" ]
+then
 cat > /etc/apt/sources.list.d/kubernetes.list << EOF
 deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/kubernetes/core:/stable:/${version}/deb/ /
 EOF
-
-INFO "cat /etc/apt/sources.list.d/kubernetes.list"
-cat /etc/apt/sources.list.d/kubernetes.list
+else
+cat > /etc/apt/sources.list.d/kubernetes.list << EOF
+deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${version}/deb/ /
+EOF
+INFO "cat /etc/apt/sources.list.d/kubernetes.list" && cat /etc/apt/sources.list.d/kubernetes.list
 
 # install
-INFO "apt-get update"
-apt-get update || exit 1
-EXEC "apt-get -y install curl apt-transport-https"
-INFO "apt-get install -y kubectl kubelet kubeadm"
-apt-get install -y kubectl kubelet kubeadm || exit 1
+INFO "apt update" && apt update || exit 1
+INFO "apt install -y kubectl kubelet kubeadm" && apt install -y kubectl kubelet kubeadm || exit 1
 EXEC "apt-mark hold kubelet kubeadm kubectl"
 
 }
 
 _centos() {
-sleep 1s
+
+cat > /etc/yum.repos.d/kubernetes.repo << EOF
+[kubernetes]
+name=kubernetes
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/kubernetes/yum/repos/kubernetes-el7-$basearch
+name=Kubernetes
+baseurl=https://mirrors.tuna.tsinghua.edu.cn/kubernetes/core:/stable:/${version}/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/${version}/rpm/repodata/repomd.xml.key
+EOF
+INFO "cat /etc/yum.repos.d/kubernetes.repo" && cat /etc/yum.repos.d/kubernetes.repo
+
 }
 
 main() {
 
 # check os
 osInfo=`get_os` && INFO "current os: $osInfo"
-! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20|ubuntu22' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20、centos7、centos8"
+! echo "$osInfo" | grep -E 'ubuntu18|ubuntu20|ubuntu22' &> /dev/null && ERROR "You could only install on os: ubuntu18、ubuntu20、ubuntu22"
+# check location
+countryCode=$(check_if_in_china)
+[ ".${countryCode}" = "." ] && ERROR "Get country location fail ..."
 
 # environments
-serviceName="kubelet"
 version="v1.28"
-# countryCode=`curl -SsL https://api.ip.sb/geoip | sed 's/,/\n/g' | grep country_code | awk -F '"' '{print $(NF-1)}'`
-curl -SsL cip.cc | grep -E '^地址' | head -1 | grep '中国' &> /dev/null && countryCode="CN" || countryCode="Other"
 
 # check if installed
 kubectl version --client &> /dev/null && kubeadm version &> /dev/null && YELLOW "kubectl and kubeadm have installed ..." && return 0
