c7d0d4697cc607a5a75f82d93617a2826239afee
Xiechengqi <Xiechengqi01@gmail.com>
2025-04-15 04:54:04 +0000

    add

--- install/Promtail/install.sh
+++ install/Promtail/install.sh
@@ -1,54 +1,55 @@
 #!/usr/bin/env bash
 
 #
+# 2025/04/15
 # xiechengqi
-# 2021/11/17
 # install promtail
+# usage: curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/install/Promtail/install.sh | sudo bash [-s LOKI_URL]
 #
 
-# source /etc/profile
+source /etc/profile
+# BASEURL="https://raw.githubusercontent.com/Xiechengqi/scripts/master"
 BASEURL="https://gitee.com/Xiechengqi/scripts/raw/master"
 source <(curl -SsL $BASEURL/tool/common.sh)
 
 main() {
+# check location
+countryCode=$(check_if_in_china)
+[ ".${countryCode}" = "." ] && ERROR "Get country location fail ..."
+INFO "Location: ${countryCode}"
+
 # check os
 osInfo=`get_os` && INFO "current os: $osInfo"
-# ! echo "$osInfo" | grep -E 'centos7|ubuntu18|ubuntu20' &> /dev/null && ERROR "You could only install on os: centos7、ubuntu18、ubuntu20"
+! echo "$osInfo" | grep -E 'centos7|ubuntu18|ubuntu20|ubuntu22' &> /dev/null && ERROR "You could only install on os: centos7、ubuntu18、ubuntu20、ubuntu22"
 
 # environments
 serviceName="promtail"
-version="2.8.1"
-installPath="/data/${serviceName}-${version}"
+version="3.4.3"
+installPath="/data/${serviceName}"
 downloadUrl="https://github.com/grafana/loki/releases/download/v${version}/promtail-linux-amd64.zip"
-ip=`hostname -I | awk '{print $1}'`
+[ "${countryCode}" = "China" ] && downloadUrl="${GITHUB_PROXY}/${downloadUrl}"
+ip=$(hostname -I | awk '{print $1}')
 LOKI_URL="$1"
 
 # check service
-systemctl is-active $serviceName &> /dev/null && YELLOW "$serviceName has been installed ..." && return 0
+systemctl is-active ${serviceName} &> /dev/null && YELLOW "${serviceName} has been installed ..." && return 0
 
 # check install path
-EXEC "rm -rf $installPath $(dirname $installPath)/${serviceName}"
-EXEC "mkdir -p $installPath/{bin,conf,logs}"
-
-# install unzip
-if [[ "$osInfo" =~ "centos" ]]
-then
-EXEC "yum install -y unzip"
-else
-EXEC "apt update && apt install -y unzip"
-fi
+EXEC "rm -rf ${installPath} $(dirname ${installPath})/${serviceName}"
+EXEC "mkdir -p ${installPath}/{bin,conf,logs}"
 
 # download
+EXEC "which unzip"
 EXEC "rm -rf /tmp/${serviceName} && mkdir /tmp/${serviceName}"
-EXEC "curl -SsL $downloadUrl -o /tmp/${serviceName}/${serviceName}.zip"
+EXEC "curl -SsL ${downloadUrl} -o /tmp/${serviceName}/${serviceName}.zip"
 EXEC "unzip /tmp/${serviceName}/${serviceName}.zip -d ${installPath}/bin"
 EXEC "mv ${installPath}/bin/promtail-linux-amd64 ${installPath}/bin/promtail"
 
 # register bin
-EXEC "ln -fs $installPath/bin/promtail /usr/local/bin/promtail"
+EXEC "ln -fs ${installPath}/bin/promtail /usr/local/bin/promtail"
 
 # conf
-cat > $installPath/conf/config.yaml << EOF
+cat > ${installPath}/conf/config.yaml << EOF
 server:
   http_listen_port: 9080
   grpc_listen_port: 0
@@ -66,21 +67,23 @@ scrape_configs:
       - localhost
     labels:
       ip: ${ip}
+      hostname: $(hostname)
       __path__: /var/log/containers/*
 EOF
 
 # create start.sh
-cat > $installPath/start.sh << EOF
+cat > ${installPath}/start.sh << EOF
 #!/usr/bin/env bash
 # source /etc/profile
 
-timestamp=\$(date +%Y%m%d-%H%M%S)
 installPath="${installPath}"
+
+timestamp=\$(date +%Y%m%d-%H%M%S)
 touch \${installPath}/logs/\${timestamp}.log && ln -fs \${installPath}/logs/\${timestamp}.log \${installPath}/logs/latest.log
 
 promtail -config.file=\${installPath}/conf/config.yaml  &> \${installPath}/logs/latest.log
 EOF
-EXEC "chmod +x $installPath/start.sh"
+EXEC "chmod +x ${installPath}/start.sh"
 
 # register service
 EXEC "rm -f /lib/systemd/system/${serviceName}.service"
@@ -93,7 +96,7 @@ After=network.target
 [Service]
 User=root
 Group=root
-ExecStart=/bin/bash $installPath/start.sh
+ExecStart=/bin/bash ${installPath}/start.sh
 ExecStop=/bin/kill -s QUIT \$MAINPID
 Restart=always
 RestartSec=2
@@ -102,20 +105,17 @@ RestartSec=2
 WantedBy=multi-user.target
 EOF
 
-# change softlink
-EXEC "ln -fs $installPath $(dirname $installPath)/$serviceName"
-
 # start
-EXEC "systemctl daemon-reload && systemctl enable $serviceName && systemctl start $serviceName"
-EXEC "systemctl status $serviceName --no-pager" && systemctl status $serviceName --no-pager
+EXEC "systemctl daemon-reload && systemctl enable ${serviceName} && systemctl start ${serviceName}"
+EXEC "systemctl status ${serviceName} --no-pager" && systemctl status ${serviceName} --no-pager
 
 # info
-YELLOW "${serviceName} version: $version"
-YELLOW "install: $installPath"
-YELLOW "config: $installPath/conf"
-YELLOW "data: $installPath/data"
-YELLOW "log: tail -f $installPath/logs/latest.log"
-YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] $serviceName"
+YELLOW "${serviceName} version: ${version}"
+YELLOW "install: ${installPath}"
+YELLOW "config: ${installPath}/conf"
+YELLOW "data: ${installPath}/data"
+YELLOW "log: tail -f ${installPath}/logs/latest.log"
+YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] ${serviceName}"
 }
 
 main $@
