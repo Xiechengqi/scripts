1eeb6e7ef6dc91620f854b260c150e1f3f9444a3
Xiechengqi <Xiechengqi01@gmail.com>
2025-04-24 15:21:14 +0800

    Update install.sh

--- install/K3S/install.sh
+++ install/K3S/install.sh
@@ -25,30 +25,29 @@ version="1.32.3"
 installPath="/data/${serviceName}"
 downloadUrl="https://github.com/k3s-io/k3s/releases/download/v${version}%2Bk3s1/k3s"
 [ "${countryCode}" = "China" ] && downloadUrl="${GITHUB_PROXY}/${downloadUrl}"
+imageDownloadUrl="https://github.com/k3s-io/k3s/releases/download/v${version}%2Bk3s1/k3s-airgap-images-amd64.tar.gz"
+[ "${countryCode}" = "China" ] && imageDownloadUrl="${GITHUB_PROXY}/${imageDownloadUrl}"
 binary="k3s"
 
 # check service
 systemctl is-active ${serviceName} &> /dev/null && YELLOW "${serviceName} is running ..." && return 0
 
-# check containerd service
-EXEC "systemctl is-active containerd"
-
 # check install path
-EXEC "rm -rf ${installPath}"
-EXEC "mkdir -p ${installPath}/{bin,data,logs}"
+EXEC "rm -rf ${installPath} /var/lib/rancher/k3s /etc/rancher/k3s"
+EXEC "mkdir -p ${installPath}/{bin,logs} /var/lib/rancher/k3s/agent/images"
+
+EXEC "ln -fs /var/lib/rancher/k3s ${installPath}/data"
+EXEC "ln -fs /etc/rancher/k3s ${installPath}/conf"
 
 # download tarball
 EXEC "curl -SsL ${downloadUrl} -o ${installPath}/bin/${binary} && chmod +x ${installPath}/bin/${binary}"
+EXEC "cd /var/lib/rancher/k3s/agent/images/"
+INFO "wget ${imageDownloadUrl}" && wget ${imageDownloadUrl} || exit 1
+EXEC "cd ${installPath}"
 
 # register bin
 EXEC "ln -fs ${installPath}/bin/${binary} /usr/local/bin/${binary}"
 EXEC "${binary} -version" && ${binary} -version
-
-# create config
-cat > ${installPath}/${serviceName}.yaml << EOF
-write-kubeconfig-mode: "0644"
-cluster-init: true
-EOF
 
 # creat start.sh
 cat > ${installPath}/start.sh << EOF
@@ -59,7 +58,7 @@ export installPath="${installPath}"
 timestamp=\$(date +%Y%m%d-%H%M%S)
 touch \${installPath}/logs/\${timestamp}.log && ln -fs \${installPath}/logs/\${timestamp}.log \${installPath}/logs/latest.log
 
-\${installPath}/bin/k3s server --write-kubeconfig \${installPath}/kubeconfig --config \${installPath}/${serviceName}.yaml --data-dir \${installPath}/data --container-runtime-endpoint unix:///var/run/containerd/containerd.sock &> \${installPath}/logs/latest.log
+\${installPath}/bin/k3s server --write-kubeconfig \${installPath}/kubeconfig &> \${installPath}/logs/latest.log
 EOF
 EXEC "chmod +x ${installPath}/start.sh"
 
@@ -87,14 +86,17 @@ EOF
 EXEC "systemctl daemon-reload && systemctl enable ${serviceName} && systemctl start ${serviceName}"
 EXEC "systemctl status ${serviceName} --no-pager" && systemctl status ${serviceName} --no-pager
 
-# copy kubeconfig
-EXEC "mkdir -p /root/.kube && cp -f ${installPath}/kubeconfig /root/.kube/config"
+# alias
+! which kubectl && ! alias kubectl && EXEC "echo \"alias kubectl=\"k3s kubectl\"\" >> /etc/profile"
+! which crictl && ! alias crictl && EXEC "echo \"alias crictl=\"k3s crictl\"\" >> /etc/profile"
+INFO "Run 'source /etc/profile' to active"
 
 # info
 YELLOW "${serviceName} version: ${version}"
 YELLOW "install: ${installPath}"
 YELLOW "log: tail -f ${installPath}/logs/latest.log"
 YELLOW "managemanet cmd: systemctl [stop|start|restart|reload] ${serviceName}"
+YELLOW "alias kubectl='k3s kubectl' && alias crictl='k3s crictl'"
 }
 
 main $@
