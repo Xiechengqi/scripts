95517e36bafbd211a5b0a616bc2609993e963b13
Xiechengqi <Xiechengqi01@gmail.com>
2025-05-02 23:08:48 +0800

    Update sn42.sh

--- tool/sn42.sh
+++ tool/sn42.sh
@@ -8,23 +8,27 @@
 # usage: sn42_tweets_number_metric [axon]
 function sn42_tweets_number_metric() {
 axon=${1}
-local teeUrl=$(curl -SsL http://${axon}/tee | sed 's/"//g')
-[ ".${teeUrl}" = "." ] && echo "Error: ${axon}" && exit 1
-local sig=$(curl -SsL -k ${teeUrl}/job/generate -H "Content-Type: application/json" -d '{ "type": "telemetry"}')
-local uuid=$(curl -SsL -k ${teeUrl}/job/add -H "Content-Type: application/json" -d '{ "encrypted_job": "'${sig}'" }' | jq -r .uid)
-local result=$(curl -SsL -k ${teeUrl}/job/status/${uuid})
-local de=$(curl -SsL -k ${teeUrl}/job/result -H "Content-Type: application/json" -d '{ "encrypted_result": "'${result}'", "encrypted_request": "'${sig}'" }')
+local teeUrl=$(curl -SsL http://${axon}/tee 2> /dev/null | sed 's/"//g')
+[ ".${teeUrl}" = "." ] && return 1
+local sig=$(curl -SsL -k ${teeUrl}/job/generate -H "Content-Type: application/json" -d '{ "type": "telemetry"}' 2> /dev/null)
+local uuid=$(curl -SsL -k ${teeUrl}/job/add -H "Content-Type: application/json" -d '{ "encrypted_job": "'${sig}'" }' 2> /dev/null | jq -r .uid)
+local result=$(curl -SsL -k ${teeUrl}/job/status/${uuid} 2> /dev/null)
+local de=$(curl -SsL -k ${teeUrl}/job/result -H "Content-Type: application/json" -d '{ "encrypted_result": "'${result}'", "encrypted_request": "'${sig}'" }' 2> /dev/null)
 local tweets=$(echo ${de} | jq '.stats.twitter_returned_tweets')
+[ ".${tweets}" = "." ] && return 1
 echo "sn42_tweets_number{axon=\"${axon}\", tee=\"${teeUrl}\"} ${tweets}"
 }
 
 # usage: sn42_tweets_per_minute_metric [axon]
 function sn42_tweets_per_minute_metric() {
 axon=${1}
-local teeUrl=$(curl -SsL http://${axon}/tee | sed 's/"//g')
+local teeUrl=$(curl -SsL http://${axon}/tee 2> /dev/null | sed 's/"//g')
+[ ".${teeUrl}" = "." ] && return 1
 local tweetsA=$(sn42_tweets_number_metric ${axon} | awk '{print $NF}')
+[ ".${tweetsA}" = "." ] && return 1
 sleep 10s
 local tweetsB=$(sn42_tweets_number_metric ${axon} | awk '{print $NF}')
+[ ".${tweetsB}" = "." ] && return 1
 local tweetsPerMinute=$(echo "$(echo "${tweetsB} - ${tweetsA}" | bc) * 6" | bc)
 echo "sn42_tweets_per_minute{axon=\"${axon}\", tee=\"${teeUrl}\"} ${tweetsPerMinute}"
 }
