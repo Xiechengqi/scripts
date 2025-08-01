e1d248d1aa11738aa5fa40ecf8b7e861683661fe
Xiechengqi <Xiechengqi01@gmail.com>
2025-08-01 11:42:17 +0800

    Create delete-ip.sh

---
+++ install/ubuntu/ufw/delete-ip.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# xiechengqi
+# 2025/08/01
+# usage: curl -SsL https://gitee.com/Xiechengqi/scripts/raw/master/install/ubuntu/ufw/delete-ip.sh | sudo bash -s [ip]
+#
+
+main() {
+
+local ip=${1}
+[ ".${ip}" = "." ] && echo "Less IP, exit ..." && exit 1
+echo "=> grep ufw ${ip}"
+ufw status numbered | grep -E " ${ip} " || exit 0
+echo "=> delete ufw ${ip}"
+for number in $(ufw status numbered | grep -E " ${ip} " | grep -oP "^\[[0-9]+\]" |  grep -oP "[0-9]+" | sort -rn)
+do
+echo "=> ufw delete ${number}"
+echo 'y' | ufw delete ${number}
+done
+
+}
+
+main $@
