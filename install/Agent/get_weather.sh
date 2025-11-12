59a7a61fc469dced30f96c810a7fb66649f5ad68
Xiechengqi <Xiechengqi01@gmail.com>
2025-11-12 18:14:58 +0800

    Create get_weather.sh

---
+++ install/Agent/get_weather.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+main() {
+
+export CITY=$(echo ${1} | awk -F '=' '{print $NF}')
+
+cat << EOF
+
+===== ${CITY} 天气 =====
+$(curl -SsL https://wttr.in/${CITY}?T)
+
+EOF
+)
+
+}
+
+main $@
